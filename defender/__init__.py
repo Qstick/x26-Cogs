37572a81f83cdc34c81ef49decfc468078419fce
Qstick <qstick@gmail.com>
2023-05-07 01:07:05 -0500

    [defender] update for 3.5

--- defender/__init__.py
+++ defender/__init__.py
@@ -1,10 +1,11 @@
 import json
 from .defender import Defender
 from pathlib import Path
+from redbot.core.bot import Red
 
 with open(Path(__file__).parent / "info.json") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
 
-def setup(bot):
-    bot.add_cog(Defender(bot))
+async def setup(bot: Red) -> None:
+    await bot.add_cog(Defender(bot))
